0d0594853bd1e96d1ca670cbceaf6272c9b89374
Jim Clark <jimhome1608@gmail.com>
2022-05-02 10:41:56 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '130238_no_password_save';
+}
